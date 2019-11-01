dfcb88ac72bc37aa5611213751d8d5e5132d462c
Izzeldin Addarda <38206617+zelsaddr@users.noreply.github.com>
2019-11-01 18:38:40 +0700

    Update run.php

--- run.php
+++ run.php
@@ -16,11 +16,11 @@ if(isset($register['success'])){
     }
     echo "[#] OTP : "; $otp = trim(fgets(STDIN));
     $send = $gojek->send_otp($register['otp_token'], $otp);
-    do{
+    while(!$send['success']){
         echo "\t[!] Invalid OTP.".PHP_EOL;
         echo "[#] OTP : "; $otp = trim(fgets(STDIN));
         $send = $gojek->send_otp($register['otp_token'], $otp);
-    }while($send['success'] == false);
+    }
     echo "\t[+] Access token for number ".$nomor." is ".$send['access_token'].PHP_EOL;
     echo "\t[$] Try to redeem GOFOODBOBA07".PHP_EOL;
     echo $gojek->redeem("GOFOODBOBA07", 0, $send['access_token'])['success'] == true ? "\t\t[$] Success redeem GOFOODBOBA07..".PHP_EOL : "\t\t[!] Failed redeem GOFOODBOBA07..".PHP_EOL;
@@ -34,6 +34,7 @@ if(isset($register['success'])){
     sleep(15);
     echo $gojek->redeem(0, "44c78b9b-69ec-4fe0-9c63-feb4e6075aed", $send['access_token'])['success'] == true ? "\t\t[$] Success redeem Mc Donald's..".PHP_EOL : "\t\t[!] Failed redeem Mc Donald's..".PHP_EOL;
     echo "\tDone Redeem..".PHP_EOL;
+    echo "\tSending Rp.1 to account : ".$gojek->send_rp1().PHP_EOL;
 }else{
     print("\t".$register['message'].PHP_EOL);
 }
